f3007442ea5f521d3ba42651d57138ce23778766
Milosska <110258611+Milosska@users.noreply.github.com>
2023-04-29 21:27:09 +0300

    lesson 4

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,45 @@
 "use strict";
-const button = document.querySelector("#button");
-button.addEventListener("click", () => {
-    console.log("Click!");
-});
+class Key {
+    constructor() {
+        this.signature = Math.floor(Math.random() * 11);
+    }
+    getSignature() {
+        return this.signature;
+    }
+}
+class Person {
+    constructor(key) {
+        this.key = key;
+    }
+    getKey() {
+        return this.key;
+    }
+}
+class House {
+    constructor(key) {
+        this.key = key;
+        this.door = false;
+        this.tenants = [];
+    }
+    comeIn(person) {
+        if (!this.door) {
+            throw new Error("The door is closed!");
+        }
+        this.tenants.push(person);
+        console.log("Person inside");
+    }
+}
+class MyHouse extends House {
+    openDoor(key) {
+        if (key.getSignature() !== this.key.getSignature()) {
+            throw new Error("Key to another door");
+        }
+        return (this.door = true);
+    }
+}
+let key1 = new Key();
+let stoneHouse = new MyHouse(key1);
+let person = new Person(key1);
+stoneHouse.openDoor(person.getKey());
+stoneHouse.comeIn(person);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,51 @@
-const button = document.querySelector("#button")!;
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.floor(Math.random() * 11);
+  }
 
-button.addEventListener("click", () => {
-  console.log("Click!");
-});
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (!this.door) {
+      throw new Error("The door is closed!");
+    }
+    this.tenants.push(person);
+    console.log("Person inside");
+  }
+  abstract openDoor(a: Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() !== this.key.getSignature()) {
+      throw new Error("Key to another door");
+    }
+
+    return (this.door = true);
+  }
+}
+
+let key1 = new Key();
+let stoneHouse = new MyHouse(key1);
+let person = new Person(key1);
+
+stoneHouse.openDoor(person.getKey());
+stoneHouse.comeIn(person);
